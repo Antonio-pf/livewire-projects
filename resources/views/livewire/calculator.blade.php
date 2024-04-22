0aadb93ef7ad540f624fbd5e9d3c74529d7a4940
Antonio-pf <antoniopfelipe03@gmail.com>
2024-04-21 21:33:14 -0300

    add model TodoItem

--- resources/views/livewire/calculator.blade.php
+++ resources/views/livewire/calculator.blade.php
@@ -1,12 +1,10 @@
+
 <div class="flex flex-col items-center">
     <div class="flex p-16 mx-auto justify-center items-center gap-4">
         <input class="bg-gray-50 border border-gray-300 text-gray-900
         text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block
         w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
         dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="number" wire:model="num1" placeholder="Number 1">
-
-
-
 
             <select wire:model="action" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0
             border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none
@@ -29,3 +27,4 @@
     <p class="text-3xl">{{$result}}</p>
 
 </div>
+
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Livewire\Calculator;
+use App\Livewire\TodoList;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/counter', function () {
     return view('welcome');
-});
+})->name('counter');
 
 //Route::get('/calculator', function () {
 //    return view('livewire.calculator', [
@@ -14,4 +15,6 @@ Route::get('/', function () {
 //    ]);
 //});
 //Route::get('/calculator', \App\Http\Livewire\Calculator::class)->name('calculator');
-Route::get('/calculator', Calculator::class);
+Route::get('/calculator', Calculator::class)->name('calculator');
+Route::get('/todo-list', TodoList::class)->name('todo-list');
+
